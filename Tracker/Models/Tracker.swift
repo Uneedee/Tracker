6890962fd868d3f2b49db72ee632ee709d008362
AlexeyRatushnyak <aratusha2602@gmail.com>
2025-11-30 21:50:31 +0300

    sprint_14 реализованы модели и методы работы с моделями

---
+++ Tracker/Models/Tracker.swift
@@ -0,0 +1,55 @@
+import UIKit
+
+struct Tracker {
+    let id: UUID
+    let title: String
+    let color: UIColor
+    let emoji: String
+    let schedule: [Weekdays]
+    
+}
+
+enum Weekdays: Int, CaseIterable {
+    case sunday = 1
+    case monday = 2
+    case tuesday = 3
+    case wednesday = 4
+    case thursday = 5
+    case friday = 6
+    case saturday = 7
+    
+    var localizedName: String {
+        switch self {
+        case .sunday: return "Воскресенье"
+        case .monday: return "Понедельник"
+        case .tuesday: return "Вторник"
+        case .wednesday: return "Среда"
+        case .thursday: return "Четверг"
+        case .friday: return "Пятница"
+        case .saturday: return "Суббота"
+        }
+    }
+    
+    var shortName: String {
+        switch self {
+        case .sunday: return "Вс"
+        case .monday: return "Пн"
+        case .tuesday: return "Вт"
+        case .wednesday: return "Ср"
+        case .thursday: return "Чт"
+        case .friday: return "Пт"
+        case .saturday: return "Сб"
+        }
+    }
+}
+
+struct TrackerCategory {
+    let title: String
+    let trackers: [Tracker]
+}
+
+struct TrackerRecord {
+    let trackerId: UUID
+    let date: Date
+    
+}
--- Tracker/TrackersListViewController.swift
+++ Tracker/TrackersListViewController.swift
@@ -1,11 +1,28 @@
 import UIKit
 
-class TrackerListViewController: UIViewController {
+class TrackersViewController: UIViewController {
+    
 
+    var categories: [TrackerCategory] = []
+    var completedTrackers: [TrackerRecord] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func addCompletedTracker(trackerID: UUID, date: Date){
+        let record = TrackerRecord(trackerId: trackerID, date: date)
+        completedTrackers.append(record)
+    }
+    
+    func removeCompletedTracker(trackerId: UUID, date: Date) {
+        completedTrackers.removeAll { completedTracker in
+            completedTracker.trackerId == trackerId && Calendar.current.isDate(completedTracker.date, inSameDayAs: date)
+        }
+    }
+    
+    func addTracker(tracker: Tracker, categoryTitle: String) {
+        
+    }
 }
